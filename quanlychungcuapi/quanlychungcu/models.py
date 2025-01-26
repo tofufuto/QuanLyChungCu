5d50918b5746c428115263bdc05beb9d84fbcb57
dungHVgithub <vandunghoang2004@gmail.com>
2025-01-27 00:49:48 +0700

    a
    a

--- quanlychungcuapi/quanlychungcu/models.py
+++ quanlychungcuapi/quanlychungcu/models.py
@@ -33,6 +33,7 @@ class NguoiDung(AbstractUser):
      sdt = models.CharField(max_length=10)
      cccd = models.CharField(max_length=12)
      birthdate = models.DateField(default=now)
+     thong_bao = models.BooleanField(default=False, verbose_name="Thông báo đơn hàng")
 
 
      class Meta:
@@ -78,8 +79,9 @@ class TheGiuXeNguoiThan(BaseModel):
 class PhanAnh(BaseModel):
     nguoi_dung = models.ForeignKey(NguoiDung, on_delete=models.CASCADE, null=False, related_name='phan_anhs')
     noi_dung = models.TextField(null=False)
+    status = models.CharField(max_length=255, null=False, choices=PhanAnhStatus.CHOICES, default=PhanAnhStatus.WAITING)
+
     tieu_de = models.TextField(null=False)
-    status = models.CharField(max_length=255,null=False,default=PhanAnhStatus.WAITING.value)
 
 class HinhAnhPhanAnh (BaseModel):
     phan_anh = models.ForeignKey(NguoiDung, on_delete=models.CASCADE, null=False, related_name='hinh_anhs')
@@ -88,6 +90,10 @@ class HinhAnhPhanAnh (BaseModel):
 class TuDoDienTu(BaseModel):
     ten_do = models.CharField(max_length=255)
     mo_ta = models.CharField(max_length=255)
+    trang_thai = models.CharField(max_length=50, choices=[
+        ('empty', 'Trống'),
+        ('stocked', 'Có hàng')
+    ], default='empty', verbose_name="Trạng thái")
     nguoi_dung = models.ForeignKey(NguoiDung,on_delete=models.PROTECT,null=False,related_name='tu_dos')
 
 class LuaChon(BaseModel):
